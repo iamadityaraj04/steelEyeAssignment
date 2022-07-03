982a08f4252610cde0f07ff0c1b78a88a4d80f51
iamadityaraj04 <adityarohitraj@gmail.com>
2022-07-03 11:19:43 +0530

    assignment

--- main.py
+++ main.py
@@ -2,40 +2,36 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
-import requests
 
-db = []
+db = [
+    {'trader': "Aditya", "trader_id":"1","instrument_id":"APPL","instrument_name":"Apple"},
+    {'trader': "Shubham", "trader_id":"2","instrument_id":"APPL","instrument_name":"Apple"},
+    {'trader': "Alok", "trader_id":"3","instrument_id":"SWGY","instrument_name":"Swiggy"},
+    {'trader': "Rahul", "trader_id":"4","instrument_id":"APPL","instrument_name":"Apple"},
+    {'trader': "Ritik", "trader_id":"5","instrument_id":"SWGY","instrument_name":"Swiggy"},
+    {'trader': "Bob", "trader_id":"6","instrument_id":"AZN","instrument_name":"Amazon"},
+    {'trader': "Yuvraj", "trader_id":"7","instrument_id":"PNP","instrument_name":"Phonepe"},
+    {'trader': "Rishav", "trader_id":"8","instrument_id":"AZN","instrument_name":"Amazon"},
+    {'trader': "Annie", "trader_id":"9","instrument_id":"APPL","instrument_name":"Apple"},
+    {'trader': "Archer", "trader_id":"10","instrument_id":"MTA","instrument_name":"Meta"},
+    {'trader': "James", "trader_id":"11","instrument_id":"MTA","instrument_name":"Meta"},
+    {'trader': "Thanos", "trader_id":"12","instrument_id":"PNP","instrument_name":"Phonepe"} 
+]
 
 class City(BaseModel):
-    name: str
-    timezone: str
-
+    trader: int
+    trader_id: str
+    instrument_id:str
+    instrument_name:str
 @app.get('/')
 def index():
-    return {'key' : 'value'}
+    return {'Hello' : 'Traders'}
 
-@app.get('/cities')
+@app.get('/traders')
 def get_cities():
-    results = []
-    for city in db:
-        r = requests.get(f'http://worldtimeapi.org/api/timezone/{city["timezone"]}')
-        current_time = r.json()['datetime']
-        results.append({'name' : city['name'], 'timezone': city['timezone'], 'current_time': current_time})
-    return results
+    return db
 
-@app.get('/cities/{city_id}')
-def get_city(city_id: int):
-    city = db[city_id-1]
-    r = requests.get(f'http://worldtimeapi.org/api/timezone/{city["timezone"]}')
-    current_time = r.json()['datetime']
-    return {'name' : city['name'], 'timezone': city['timezone'], 'current_time': current_time}
+@app.get('/traders/{trader_id}')
+def get_city(trader_id: int):
+    return db[trader_id-1] 
 
-@app.post('/cities')
-def create_city(city: City):
-    db.append(city.dict())
-    return db[-1]
-
-@app.delete('/cities/{city_id}')
-def delete_city(city_id: int):
-    db.pop(city_id-1)
-    return {}
